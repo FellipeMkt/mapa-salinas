7ccb7aa4c68ca3c7fb7a35d1c3e9b87778b88acf
FellipeMkt <fellipemelomkt@outlook.com>
2025-03-16 00:58:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var map = L.map('map', {
 });
 
 // Definir as dimensões da imagem do mapa
-var w = 3500, h = 2453; // Ajuste para o tamanho real da sua imagem
+var w = 100%, h = 100%; // Ajuste para o tamanho real da sua imagem
 var bounds = [[0, 0], [h, w]];
 
 // Adicionar a imagem do mapa
